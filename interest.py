8eded232249c848537aa9eb89a42cb2465e4211c
guruseelank <guruseelankabio@gmail.com>
2024-09-24 16:59:41 +0530

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,8 @@
+def simple_interest(p,t,r):
+    print("the principle is",p)
+    print("the time period is",t)
+    print("the rate of interest is",r)
+    si=(p*t*r)/100
+    print("the simple interest id s",si)
+    return si
+simple_interest(8,6,8)
